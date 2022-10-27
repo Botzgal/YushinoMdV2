f87114af93bb638eaec806f4b76a26288e3db893
galuh <112651537+Botzgal@users.noreply.github.com>
2022-10-27 18:08:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -80,38 +80,38 @@ const readMore = more.repeat(4001)
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ NO OWNER ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.owner = [
-['6282376267531', 'Aisyah', true],
-['6281361281833', 'Aldi', true]
+['6285804081500', 'Galuh', true],
+['6287817385492', 'Nura', true]
 ]
 global.mods = []
 global.prems = []
-global.nomorbot = '6283838186170'
-global.nomorown = '6281361281833'
-global.nomorown1 = '6281361281833'
-global.nomorown2 = '6282376267531'
-global.nomorwa = '0'
+global.nomorbot = '6287817385492'
+global.nomorown = '6285804081500'
+global.nomorown1 = '6285804081500'
+global.nomorown2 = '6287817385492'
+global.nomorwa = '6285804081500'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ WM ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.readMore = readMore
-global.author = 'Aʟᴅɪ X Aɪsʏᴀʜ'
-global.namebot = '​ 𝐘𝐮𝐬𝐡𝐢𝐧𝐨 𝐌𝐝'
-global.wm = '© 𝘾𝙧𝙚𝙖𝙩𝙚𝙙 𝘽𝙮 𝐘𝐮𝐬𝐡𝐢𝐧𝐨 𝐌𝐝'
+global.author = '𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖌𝖆𝖑'
+global.namebot = '​ 𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖈𝖔𝖒𝖊𝖑🎐'
+global.wm = '© 𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖌𝖆𝖑'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
-global.wm = '𝒀𝒖𝒔𝒉𝒊𝒏𝒐 𝑴𝒅 '
+global.wm = '𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖌𝖆𝖑 '
 global.watermark = wm
-global.wm2 = '𝒀𝒖𝒔𝒉𝒊𝒏𝒐 𝑴𝒅 '
+global.wm2 = '𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖈𝖔𝖒𝖊𝖑🎐'
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `𝑨𝒍𝒅𝒊 𝐗 𝑨𝒊𝒔𝒚𝒂𝒉`
-global.stickpack = '𝑨𝒍𝒅𝒊 𝐗 𝑨𝒊𝒔𝒚𝒂𝒉'
+global.titlebot = `𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖈𝖔𝖒𝖊𝖑🎐`
+global.stickpack = '𝖇𝖆𝖇𝖞 𝖇𝖔𝖙𝖟 𝖈𝖔𝖒𝖊𝖑🎐'
 global.stickauth = `© Yushino MD`
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ OWNER INFO ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.nameown1= 'Aldi'
-global.nameown2 = 'Aisyah'
+global.nameown1= 'Galuh'
+global.nameown2 = 'Nura'
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ MEDIA LINK ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 global.Linkgc = 'Bentar'
 global.lynk = 'https://lynk.id/hyuuraofc'
